1b6cce5533994f18f7b4afb53e362c9409343121
Eswin <winnapitupulu@gmail.com>
2022-12-16 04:32:48 +0700

    UpdateBelajarJava

--- UpdateTugas/java-oop/src/MainLogic.java
+++ UpdateTugas/java-oop/src/MainLogic.java
@@ -8,6 +8,13 @@ import logic.Logic01Soal07;
 import logic.Logic01Soal08;
 import logic.Logic01Soal09;
 import logic.Logic01Soal10;
+import logic.Logic02Soal01;
+import logic.Logic02Soal02;
+import logic.Logic02Soal03;
+import logic.Logic02Soal04;
+import logic.Logic02Soal05;
+import logic.Logic02Soal06;
+import logic.Logic02Soal07;
 import logic.Logic02Soal09;
 
 public class MainLogic {
@@ -48,9 +55,37 @@ public class MainLogic {
 //        Logic01Soal09 logic01Soal09 = new Logic01Soal09(9);
 //        logic01Soal09.cetakArray();
 
-        System.out.println("Sample Logic 01 Soal 10");
-        Logic01Soal10 logic01Soal10 = new Logic01Soal10(9);
-        logic01Soal10.cetakArray();
+//        System.out.println("Sample Logic 01 Soal 10");
+//        Logic01Soal10 logic01Soal10 = new Logic01Soal10(9);
+//        logic01Soal10.cetakArray();
+
+        System.out.println("\n\nSample Logic 02 Soal 01");
+        Logic02Soal01 logic02Soal01 = new Logic02Soal01(9);
+        logic02Soal01.cetakArray();
+
+        System.out.println("\n\nSample Logic 02 Soal 02");
+        Logic02Soal02 logic02Soal02 = new Logic02Soal02(9);
+        logic02Soal02.cetakArray();
+
+        System.out.println("\n\nSample Logic 02 Soal 03");
+        Logic02Soal03 logic02Soal03 = new Logic02Soal03(9);
+        logic02Soal03.cetakArray();
+
+        System.out.println("\n\nSample Logic 02 Soal 04");
+        Logic02Soal04 logic02Soal04 = new Logic02Soal04(9);
+        logic02Soal04.cetakArray();
+
+        System.out.println("\n\nSample Logic 02 Soal 05");
+        Logic02Soal05 logic02Soal05 = new Logic02Soal05(9);
+        logic02Soal05.cetakArray();
+
+        System.out.println("\n\nSample Logic 02 Soal 06");
+        Logic02Soal06 logic02Soal06 = new Logic02Soal06(9);
+        logic02Soal06.cetakArray();
+
+        System.out.println("\n\nSample Logic 02 Soal 07");
+        Logic02Soal07 logic02Soal07 = new Logic02Soal07(9);
+        logic02Soal07.cetakArray();
 
 //        System.out.println("\n\nSample Logic 02 Soal 09");
 //        Logic02Soal09 logic02Soal09 = new Logic02Soal09(9);
--- UpdateTugas/java-oop/src/logic/Logic02Soal02.java
+++ UpdateTugas/java-oop/src/logic/Logic02Soal02.java
@@ -6,7 +6,14 @@ public class Logic02Soal02 extends BasicLogic{
     }
 
     public void isiArray(){
-
+        for(int i = 0; i< this.n; i++){
+            for(int j = 0; j< this.n; j++){
+                if(j==0) this.array[i][j]= String.valueOf(1);
+                else if(i==0 && j>=1) this.array[i][j]= String.valueOf(Integer.parseInt(this.array[i][j-1])+2);
+                else if(j == n-1 || i == n-1) this.array[i][j] = this.array[0][j];
+                else this.array[i][j] = ".";
+            }
+        }
     }
 
     public void cetakArray(){
